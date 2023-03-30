d726bd562261ec2ecfc5cd888f2dde0de5c4b5fe
EdwinOrtegaK <andyortegakou@yahoo.com.mx>
2023-03-29 20:41:58 -0600

    Reader hecho

--- src/Reader.java
+++ src/Reader.java
@@ -1,2 +1,23 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
 public class Reader {
+    public static ArrayList<String> leer() {
+        ArrayList<String> lineas = new ArrayList<>();
+
+        try {
+            File archivo = new File("diccionario.txt");
+            Scanner scan = new Scanner(archivo);
+
+            while (scan.hasNextLine()) {
+                String linea = scan.nextLine();
+                lineas.add(linea);
+            }
+            scan.close();
+
+        } catch (Exception e) {
+            System.out.println("Error en: " + e.getMessage());
+        }
+        return lineas;
+    }
 }
