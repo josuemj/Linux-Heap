27ed2ca589be0c93815aa2102664555531ce68d5
EdwinOrtegaK <andyortegakou@yahoo.com.mx>
2023-04-03 20:52:08 -0600

    JavaDoc

---
+++ JavaDoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Controller"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"MinPriorityQueue"},{"p":"<Unnamed>","l":"Proceso"},{"p":"<Unnamed>","l":"Reader"}];updateSearchResults();
